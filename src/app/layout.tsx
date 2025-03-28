b3462c1d93717db82fbad7a56f90c2e9cabbb3d0
yejonz <yejonz@gmail.com>
2025-03-28 16:39:53 -0500

    added website icon and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import LastActiveUpdater from "@/my_components/lastActiveUpdater";
 import "./globals.css";
 import MyNavigationMenu from "@/my_components/myNavigationMenu"
+import { Metadata } from "next";
+
+export const metadata : Metadata = {
+  title: 'Poke Pocket Market'
+}
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
