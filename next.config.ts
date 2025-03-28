d2bd1dc42274ff2dc0be2afeafcdd76a84c395bd
yejonz <yejonz@gmail.com>
2025-03-28 01:03:51 -0500

    same as prev

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
